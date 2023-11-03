811923ba64cd045f3b5e4a4ec6d29a4885d2631c
sinulingga23 <sinulinggatwo@gmail.com>
2023-11-03 17:06:53 +0700

    update

--- jdbc-postgres/src/main/java/org/sinulingga/Main.java
+++ jdbc-postgres/src/main/java/org/sinulingga/Main.java
@@ -6,6 +6,7 @@ import org.sinulingga.db.ConnectionDB;
 import org.sinulingga.model.Category;
 
 import java.sql.Connection;
+import java.util.List;
 import java.util.UUID;
 
 public class Main {
@@ -13,6 +14,17 @@ public class Main {
        Connection connection = ConnectionDB.getConnection();
 
        CategoryDao categoryDao = new CategoryDao(connection);
+
+       // add category
        categoryDao.add(new Category(UUID.randomUUID().toString(), "Category 1", "Description 1"));
+
+       // find all category
+       List<Category> listCategory = categoryDao.findAll();
+
+       // find a category
+       Category category = categoryDao.findOne(listCategory.get(0).getId());
+
+       // delete a category
+       categoryDao.deleteById(category.getId());
     }
 }
--- jdbc-postgres/src/main/java/org/sinulingga/dao/CategoryDao.java
+++ jdbc-postgres/src/main/java/org/sinulingga/dao/CategoryDao.java
@@ -2,10 +2,12 @@ package org.sinulingga.dao;
 
 import org.sinulingga.model.Category;
 
-import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryDao {
     private Connection connection;
@@ -25,7 +27,70 @@ public class CategoryDao {
             preparedStatement.execute();
         } catch (SQLException e) {
             System.out.println("Error at " + CategoryDao.class.getSimpleName() + ". Error: " + e.getMessage());
-            e.getStackTrace();
+            e.printStackTrace();
+        }
+    }
+
+    public Category findOne(String id) {
+        try {
+            String query = "SELECT id, name, description FROM public.category WHERE id = ?";
+            PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.setString(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+
+            if (resultSet.next()) {
+                Category category = new Category();
+                category.setId(resultSet.getString("id"));
+                category.setName(resultSet.getString("name"));
+                category.setDescription(resultSet.getString("description"));
+                return category;
+            }
+
+            return null;
+        } catch (SQLException e) {
+            System.out.println("Error ar " + CategoryDao.class.getSimpleName() + ". Error: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Category> findAll() {
+        try {
+            String query = "SELECT id, name, description FROM public.category";
+            PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<Category> listCategory = new ArrayList<>();
+            while (resultSet.next()) {
+                Category category = new Category();
+                category.setId(resultSet.getString("id"));
+                category.setName(resultSet.getString("name"));
+                category.setDescription(resultSet.getString("description"));
+                listCategory.add(category);
+            }
+
+            return listCategory.size() > 0 ? listCategory : null;
+        } catch (SQLException e) {
+            System.out.println("Error ar " + CategoryDao.class.getSimpleName() + ". Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void deleteById(String id) {
+        try {
+            String query = "DELETE FROM public.category WHERE id = ?";
+            PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.setString(1, id);
+
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            System.out.println("Error ar " + CategoryDao.class.getSimpleName() + ". Error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
